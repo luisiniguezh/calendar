53bbb2816538df87b3d344b282985c0d51702621
luisiniguezh <luis.iniguezh@gmail.com>
2019-05-04 13:19:19 -0500

    initial commit wiith calendar dst bug fix

---
+++ calendar_dst_bug_fix/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Calendar DST bug fix',
+    'summary': """This fix displays recurring calendar events correctly after DST change""",
+    'version': '1.0.0.0.0',
+    'author': "Luis Iñiguez, "
+              "Odoo Community Association (OCA)",
+    'website': 'https://github.com/OCA/calendar',
+    'license': '',
+    'category': 'Extra Tools',
+    'depends': [
+        'base',
+        'calendar',
+    ],
+    'installable': True,
+}
